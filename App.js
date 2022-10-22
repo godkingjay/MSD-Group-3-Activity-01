03a7495c553a88476f5c2d3cd8c2f3a0368f658b
Jarrian <glorianagojar@gmail.com>
2022-10-22 17:58:54 +0800

    ADD: Font Loader and Splash Screen

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
 import globalStyles from './assets/styles/global';
 import { Text, View } from 'react-native';
+import { group } from './assets/js/objects';
+import { useCallback, useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const getFonts = () => Font.loadAsync({
+  'Varela': require('./assets/fonts/VarelaRound-Regular.ttf')
+});
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await getFonts();
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch(e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+    setAppIsReady(true);
+  }, []);
+
+  const onLayoutRootView = useCallback( async () => {
+    if(appIsReady){
+      await SplashScreen.hideAsync();
+    }
+  } ,[appIsReady])
+
+  if(!appIsReady) {
+    return null;
+  }
+
+  return(
+    <View
+      style={globalStyles.screen}
+      onLayout={onLayoutRootView}
+    >
+      <Text>Hello</Text>
     </View>
   );
 }
--- assets/styles/global.js
+++ assets/styles/global.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 
 const globalStyles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
