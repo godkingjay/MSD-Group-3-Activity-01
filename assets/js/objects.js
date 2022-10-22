80e2eb9ab2480b140885ef107eeead3eb5d8cedd
Jarrian <glorianagojar@gmail.com>
2022-10-22 17:26:12 +0800

    ADD: Group

---
+++ assets/js/objects.js
@@ -0,0 +1,11 @@
+export const group = {
+  groupAlias: 'Group Alias',
+  groupNumber: 4,
+  degree: 'Bachelor of Science in Computer Science',
+  groupMember: [
+    'Jarrian Vince G. Gojar',
+    'Jake Gamab',
+  ],
+  projectTitle: 'Authentic Pinoy Recipes',
+  projectCategory: 'Recipe'
+} 
